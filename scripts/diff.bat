3df81037ed38729fa5c81e2d683dff72110ba60b
David Miller <dmiller2718@gmail.com>
2024-05-30 11:24:19 -0400

    Add script to compare clojure lib files for changes

---
+++ scripts/diff.bat
@@ -0,0 +1,30 @@
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\gitlibs.cljc                       C:\work\clojure\clr.tools.gitlibs\src\main\clojure\clojure\tools\gitlibs.cljc
+
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\gitlibs\config.cljr                C:\work\clojure\clr.tools.gitlibs\src\main\clojure\clojure\tools\gitlibs\config.cljr
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\gitlibs\impl.cljr                  C:\work\clojure\clr.tools.gitlibs\src\main\clojure\clojure\tools\gitlibs\impl.cljr
+
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps.cljc                          C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps.cljc
+
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\extensions.cljc               C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\extensions.cljc
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\specs.cljc                    C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\specs.cljc
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\tool.cljc                     C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\tool.cljc
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\tree.cljc                     C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\tree.cljc
+
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\util\concurrent.clj           C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\util\concurrent.clj
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\util\concurrent.cljr          C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\util\concurrent.cljr
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\util\dir.cljc                 C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\util\dir.cljc
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\util\io.cljc                  C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\util\io.cljc
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\util\session.cljc             C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\util\session.cljc
+
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\script\make_classpath2.cljc   C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\script\make_classpath2.cljc
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\script\parse.cljc             C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\script\parse.cljc
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\script\resolve_tags.cljc      C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\script\resolve_tags.cljc 
+
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\extensions\deps.cljc          C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\extensions\deps.cljc 
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\extensions\git.cljc           C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\extensions\git.cljc 
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\deps\extensions\local.cljc         C:\work\clojure\clr.tools.deps\src\main\clojure\clojure\tools\deps\extensions\local.cljc
+
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\cli.cljc                           C:\work\clojure\tools.cli\src\main\clojure\clojure\tools\cli.cljc
+
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\cli\api.cljc                       C:\work\clojure\clr.tools.deps.cli\src\main\clojure\clojure\tools\deps\cli\api.cljc
+fc C:\work\clojure\deps.cljr\src\dotnet\cljr\clojure\tools\cli\help.cljc                      C:\work\clojure\clr.tools.deps.cli\src\main\clojure\clojure\tools\deps\cli\help.cljc
